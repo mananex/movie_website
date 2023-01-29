4ac304431a93b90a87411428a4dc5c022c274acf
mananex <84662402+mananex@users.noreply.github.com>
2023-01-29 18:51:56 +0400

    Delete test.py

--- parsing/test.py
+++
@@ -1,3 +0,0 @@
-sql_command = """INSERT INTO movies (title, picture, description, runtime, release_date, created_at) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')"""
-print(sql_command.format('title1', 'picture3', 'description___/', 'runtime_213', 'release_date99', 'created_attt'))
-
